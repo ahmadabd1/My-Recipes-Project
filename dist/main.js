35d5e0596943e912c2f2dcce74a7fc421aafe2c3
Ahmad Abdallah <ahmadabdallah174@gmail.com>
2023-12-18 12:20:57 +0200

    finish the prject like the description still to add extention

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 const sourceRecipes = $("#resultsRecipes-template").html()
 const templateRecipes = Handlebars.compile(sourceRecipes)
-let val;
+
 
 
 const displayTheRecipes = function (dataRecipes) {
@@ -12,23 +12,29 @@ const displayTheRecipes = function (dataRecipes) {
 
 
 $("#results").on("click", ".card-img-top", function () {
-    $.get(`/recipes/${val}`).then(data => {
+    let inputValue = $(".search-box").val()
+    var checkBoxGluten = $("#Gluten").is(':checked');
+    var checkBoxDietary = $("#Dietary").is(':checked');
+    $.get(`/recipes/${inputValue}?gluten=${checkBoxGluten}&dieary=${checkBoxDietary}`).then(data => {
+        
         console.log(data)
         const searchObject = data.find((ind) => ind.imgUrl == $(this).attr('src'));
         alert(searchObject.ingredients[0])
+        // displayTheRecipes(data)
     })
 })
 
 
 
+
 $("#searchBtn").on("click", () => {
     let inputValue = $(".search-box").val()
-    val = inputValue
+    
     var checkBoxGluten = $("#Gluten").is(':checked');
     var checkBoxDietary = $("#Dietary").is(':checked');
 
     $.get(`/recipes/${inputValue}?gluten=${checkBoxGluten}&dieary=${checkBoxDietary}`).then(data => {
-        // myFunction(data)
+       
         displayTheRecipes(data)
     })
 
