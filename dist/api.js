21b8fe4cc797af8353a64be3a008ee76ff0e1c5f
Ahmad Abdallah <ahmadabdallah174@gmail.com>
2023-12-19 17:03:51 +0200

    add moduel frontend

--- dist/api.js
+++ dist/api.js
@@ -1,10 +1,5 @@
 class APIModuel{
 
- 
-        constructor() {
-            // this.data = {}
-        }
-    
         getAllData(ingredient, gluten, dairy) {
            return $.get(`/recipes/${ingredient}?dairy=${dairy}&gluten=${gluten}`)
         }
